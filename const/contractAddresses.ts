7584cff4233e12190187c6bf6d8e720846e56d24
HashbrownKazang <admin@spuntentertainment.com>
2023-08-09 10:18:23 -0400

    saved

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -10,15 +10,8 @@ export const NETWORK = Polygon;
 export const MARKETPLACE_ADDRESS = "0x88B429Ff63aF913bbEf13a83441aF93Cde510761";
 
 // 3. The address of your NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS = [
-  "0x58a127F40b76D864B3154dFFC3cee3761FFa1729",
-  "0x1c9Fa0976A21B9b139640DE7b6360dfE5080aB62",
-  "0xa53e75922522E47FC465Ac4944691976E974376A",
-  "0x3eB42feb78DB9f4bCf4bB10241cc03BCa95e79e3",
-  "0x087539F517f622F3626a17b56a1E08bc201eed34",
-  "0x6573CDBeD7ef027cDf26bE7495Db9e11c810D90e",
-  "0xd64a8aE73FEd10F47BD2070Ced225731A894a06A",
-];
+export const NFT_COLLECTION_ADDRESS =
+  "0x3eB42feb78DB9f4bCf4bB10241cc03BCa95e79e3";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use polygonscan.com to view transactions on the Polygon Mainnet.
